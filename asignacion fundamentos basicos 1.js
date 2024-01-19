cee1f956939a3f3f3c27caef71ac7984b06d7c12
cristianantinao <cristiangabrielantinao000@gmail.com>
2024-01-19 13:04:35 -0300

    actualizacion comentarios

--- asignacion fundamentos basicos 1.js
+++ asignacion fundamentos basicos 1.js
@@ -1,4 +1,5 @@
 console.log ('1.Obten del 1 al 255:');
+/* Escribe una función que devuelve un array con todos los números del 1 al 255. */
 function get_array(){
     var arr = [];
     for(var i=1 ; i<256 ; i++){
@@ -11,6 +12,9 @@ console.log(get_array());
 console.log('-----------------');
 
 console.log('2.consigue pares hasta el 1000:');
+/* Escribe una función que entregue la suma de todos los números pares 
+del 1 al 1000 - Puedes usar un operador de módulo para este ejercicio.  */
+
 function sum_even_numbers(){
     var sum = 0;
     for (var i=1;i<1001;i++){
@@ -23,7 +27,10 @@ console.log(sum_even_numbers());
 
 console.log('-----------------');
 
-console.log('3.consigue impares hasta el 5000:');
+console.log('3.suma impares hasta el 5000:');
+/* Escribe una función que devuelva la suma de todos los números 
+impares entre 1 y 5000 (ej: 1+3+5+...+4997+4999). */
+
 function sum_odd_numbers(){
     var sum=0;
     for( var i=1; i<5001; i++){
@@ -37,6 +44,9 @@ console.log(sum_odd_numbers());
 console.log('-----------------');
 
 console.log('4.itera un Array:');
+/* Escribe una función que devuelva la suma de todos los valores dentro 
+de un array (ej:  [1,2,5] retorna 8. [-5,2,5,12] retorna 14).  */
+
 function iterateArr(arr){
     var sum=0;
     for (var i=0;i<arr.length;i++){
@@ -49,6 +59,9 @@ console.log(iterateArr([1,2,5]));
 console.log('-----------------');
 
 console.log('5.encuentra el mayor(max):');
+/* Dado un array con múltiples valores, escribe una función que devuelva el
+ número mayor (ej: para [-3,3,5,7] el número mayor (max) es 7).  */
+
 function findmax(arr){
     var max=0;
     for( var i=0;i<arr.length;i++){
@@ -63,6 +76,9 @@ console.log(findmax([-1,-300,-2,-200,-5]))
 console.log('-----------------');
 
 console.log('6.Encuentra el promedio(avg):');
+/* Dado un array con múltiples valores, escribe una función que devuelva 
+el promedio de los valores (ej: para [1,3,5,7,20] el promedio es 7.2). */
+
 function findavg(arr){
     var sum=0;
     for(var i=0;i<arr.length;i++){
@@ -76,6 +92,9 @@ console.log(findavg([1,3,5,7,20]));
 console.log('-----------------');
 
 console.log('7.Array de impares:')
+/* Escribe una función que devuelva un array de todos los números impares entre 1 y 50 
+(ej: [1,3,5, …, 47,49]). Pista: Usa el método ‘push’. */
+
 function oddnumbers(){
     var arr=[];
     for(var i=1;i<51;i++){
@@ -89,6 +108,10 @@ console.log(oddnumbers());
 console.log('-----------------');
 
 console.log('8.Mayor que Y:');
+/* Dado un valor Y, escribe una función que toma un array y devuelve los valores 
+mayores que Y. Por ejemplo, si arr = [1,3,5,7] y Y = 3,
+ tu función devolverá 2 (hay 2 números en el array mayores que 3, esto son 5 y 7).  */
+
 function greaterY(arr, Y){
     var count=0;
     for (var i=0;i<arr.length;i++){
@@ -102,6 +125,9 @@ console.log(greaterY([1,3,5,7], 3));
 console.log('-----------------');
 
 console.log('9.Cuadrados:');
+/* Dado un array con múltiples valores, escribe una función que reemplace cada 
+valor por el cuadrado del mismo valor (ej: [1,5,10,-2] será [1,25,100,4]).*/
+
 function squarevalue(arr){
     for(var i=0;i<arr.length;i++){
         arr[i]=arr[i]*arr[i];
@@ -113,6 +139,10 @@ console.log(squarevalue([1,5,10,-2]));
 console.log('-----------------');
 
 console.log('10.Negativos:');
+/*  Dado un array con múltiples valores, escribe una función que reemplace cualquier número
+ negativo dentro del array por 0. Cuando el programa esté listo, el array no debiera contener 
+ números negativos (ej: [1,5,10,-2] se convertirá en [1,5,10,0]). */
+
 function nonegative(arr){
     for(var i=0;i<arr.length;i++){
         if(arr[i]<0)
@@ -125,6 +155,10 @@ console.log(nonegative([1,5,10,-2]));
 console.log('-----------------');
 
 console.log('11.Max/Min/Avg:');
+/*  Dado un array con múltiples valores, escribe una función que devuelva un nuevo array que 
+solo contenga el valor mayor (max), menor (min) y promedio (avg) del array original 
+(ej: [1,5,10,-2] devolverá [10,-2,3.5]). */
+
 function maxminavg(arr){
     var max=0
     var min=0
@@ -147,4 +181,33 @@ function maxminavg(arr){
 }
 
 console.log(maxminavg([1,5,10,-2]))
+console.log('-----------------');
+
+console.log('12.Intercambia Valores:');
+/* Escribe una función que intercambie el primer y el último valor de cualquier array. 
+La extensión mínima predeterminada del array es 2 
+(ej: [1,5,10,-2] será [-2,5,10,1]).  */
+function swap(arr){
+    var firstvalue=arr[0];
+    arr[0]=arr[arr.length-1];
+    arr[arr.length-1]=firstvalue;
+    return arr;
+}
+
+console.log(swap([1,5,10,-2]));
+console.log('-----------------');
+
+console.log('13.De numero a string:');
+/* Escribe una función que tome un array de números y reemplace cualquier valor negativo por el string ‘Dojo’.
+ Por ejemplo, dado el array = [-1,-3,2], tu función devolverá [‘Dojo’,‘Dojo’,2]. */
+function numbertostring(arr){
+    var x='DOJO';
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]<0)
+        arr[i]="Dojo";
+    }
+    return arr;
+}
+
+console.log(numbertostring([-1,-3,2]));
 console.log('-----------------');
